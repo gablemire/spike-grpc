408938d761e497e9e0ba2ab9667228d75febad8e
Gabriel Lemire <gabriel.lemire@unity3d.com>
2022-07-13 15:28:00 -0400

    Remove usage of service from protobufjs

--- src/index.js
+++ src/index.js
@@ -8,16 +8,48 @@ const { promisify } = require("node:util");
 const { randomUUID } = require("node:crypto");
 const { grpctest } = require("./generated/grpc-test");
 
-function waitForClientReady(client) {
+function serializeRequest(request, RequestType) {
+  const validationErrors = RequestType.verify(request);
+  if (!!validationErrors) {
+    throw new Error(
+      `${RequestType.name} validation error: ${validationErrors}`
+    );
+  }
+
+  return RequestType.encode(request).finish();
+}
+
+function deserializeResponse(responseData, ResponseType) {
+  const message = ResponseType.decode(responseData);
+  return message.toJSON();
+}
+
+function msg(client, request) {
   return new Promise((resolve, reject) => {
-    const deadline = new Date().getTime() + 5000;
-    client.waitForReady(deadline, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
+    const metadata = new Metadata({
+      cacheableRequest: true,
+      idempotentRequest: true,
+      waitForReady: true,
     });
+    metadata.add("RequestId", randomUUID());
+
+    client.makeUnaryRequest(
+      `/grpctest.GrpcTest/Msg`,
+      (request) => serializeRequest(request, grpctest.MsgRequest),
+      (response) => deserializeResponse(response, grpctest.MsgReply),
+      {
+        server: "Toto",
+      },
+      metadata,
+      {},
+      (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      }
+    );
   });
 }
 
@@ -25,34 +57,9 @@ async function main() {
   const client = new Client("127.0.0.1:5551", credentials.createInsecure());
 
   try {
-    await waitForClientReady(client);
-
     console.log("Client is ready");
 
-    const service = grpctest.GrpcTest.create((method, data, callback) => {
-      console.log("Service method", method.name);
-
-      const metadata = new Metadata({
-        cacheableRequest: true,
-        idempotentRequest: true,
-        waitForReady: true,
-      });
-      metadata.add("RequestId", randomUUID());
-
-      client.makeUnaryRequest(
-        `/grpctest.${grpctest.GrpcTest.name}/${method.name}`,
-        (v) => v,
-        (v) => v,
-        data,
-        metadata,
-        {},
-        (err, response) => {
-          callback(err, response);
-        }
-      );
-    });
-
-    const response = await service.msg({
+    const response = await msg(client, {
       server: "Potato",
     });
 
